fbaa59cacb840faa92d873be6eae37ac5b62ee11
vanlinh00 <vanlinh20192019@gmail.com>
2026-07-28 23:50:49 +0700

    no message

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,10 @@ import { dataBoxIcon, dataMenu } from './constants';
 import BoxIcon from './BoxIcon/BoxIcon';
 import styles from './styles.module.scss';
 import Menu from './Menu/Menu';
+import Logo from '@icons/images/Logo-retina.png';
+import reLoadIcon from '@icons/svgs/reloadIcon.svg';
+import heartIcon from '@icons/svgs/heart.svg';
+import cartIcon from '@icons/svgs/cartIcon.svg';
 
 function MyHeader() {
     const { containerBoxIcon, containerMenu, containerBox, containerHeader } =
@@ -18,6 +22,45 @@ function MyHeader() {
                     {dataMenu.slice(0, 3).map(item => {
                         return <Menu content={item.content} href={item.href} />;
                     })}
+                </div>
+                <div>
+                    <img
+                        src={Logo}
+                        alt='Logo'
+                        style={{
+                            width: '153px',
+                            height: '53px'
+                        }}
+                    />
+                </div>
+                <div className={containerBox}>
+                    <div className={containerMenu}>
+                        {dataMenu.slice(3, dataMenu.length).map(item => {
+                            return (
+                                <Menu content={item.content} href={item.href} />
+                            );
+                        })}
+                    </div>
+                </div>
+                <div className={containerBoxIcon}>
+                    <img
+                        width={26}
+                        height={26}
+                        src={reLoadIcon}
+                        alt='reLoadIcon'
+                    />
+                    <img
+                        width={26}
+                        height={26}
+                        src={heartIcon}
+                        alt='reLoadIcon'
+                    />
+                    <img
+                        width={26}
+                        height={26}
+                        src={cartIcon}
+                        alt='reLoadIcon'
+                    />
                 </div>
             </div>
         </div>
