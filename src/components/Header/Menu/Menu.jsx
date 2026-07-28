274fa4fd6f7db2fcd0ac2e5897afb58389a98603
vanlinh00 <vanlinh20192019@gmail.com>
2026-07-28 23:10:50 +0700

    no message

--- src/components/Header/Menu/Menu.jsx
+++ src/components/Header/Menu/Menu.jsx
@@ -1,23 +1,27 @@
 import { useContext } from 'react';
 import styles from '../styles.module.scss';
-import { SideBarContext } from '@/contexts/SideBarProvider';
-import { StoreContext } from '@/contexts/storeProvider';
+// import { SideBarContext } from '@/contexts/SideBarProvider';
+// import { StoreContext } from '@/contexts/storeProvider';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Menu({ content, href }) {
     const { menu, subMenu } = styles;
-    const { setIsOpen, setType } = useContext(SideBarContext);
-    const { userInfo, handleLogOut } = useContext(StoreContext);
-    const [isShowSubMenu, setIsShowSubMenu] = useState(false);
-    const navigate = useNavigate();
+    // const { setIsOpen, setType } = useContext(SideBarContext);
+    // const { userInfo, handleLogOut } = useContext(StoreContext);
+    // const [isShowSubMenu, setIsShowSubMenu] = useState(false);
+    // const navigate = useNavigate();
 
     const handleRenderText = content => {
-        if (content === 'Sign in' && userInfo) {
-            return `Hello: ${userInfo?.username}`;
-        } else {
-            return content;
-        }
+        // if (content === 'Sign in' && userInfo) {
+        //     return `Hello: ${userInfo?.username}`;
+        // } else {
+        //     return content;
+        // }
+        return content;
     };
-    return <div>{handleRenderText(content)}</div>;
+
+    return <div> {handleRenderText(content)}</div>;
 }
+
+export default Menu;
