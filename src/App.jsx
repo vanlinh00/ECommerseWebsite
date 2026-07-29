41ac298b3c30f090d25c258cc173db36685afb42
vanlinh00 <vanlinh20192019@gmail.com>
2026-07-29 23:04:56 +0700

    no message

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
-
-import MyFooter from '@components/Footer/Footer';
-import MainLayout from '@components/Layout/Layout';
-import MyButton from '@components/Button/Button';
-import MyHeader from '@components/Header/Header';
-
+import HomePage from '@components/HomePage/HomePage';
+x;
 function App() {
     return (
         <>
-            <MainLayout>
-                <MyHeader />
-                Content
-                {/* <MyFooter /> */}
-            </MainLayout>
+            <HomePage />
         </>
     );
 }
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,30 @@
+import Button from '../Button/Button';
+import styles from './styles.module.scss';
+
+function Banner() {
+    const { container, content, title, des } = styles;
+    return (
+        <div className={container}>
+            <div className={content}>
+                <h1 className={title}>
+                    XStore Marseille04
+                    Demodddddddddddddddddddddddddddddddddddddddddddddddd
+                </h1>
+                <div className={des}>
+                    Make yours celebrations even more special this years with
+                    beautiful.
+                </div>
+
+                <div
+                    style={{
+                        width: '172px'
+                    }}
+                >
+                    {/* <Button content={'Go to shop'} /> */}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Banner;
