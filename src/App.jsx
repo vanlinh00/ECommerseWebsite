9b350c6617ca90d97dce507da7248bf883483113
vanlinh00 <vanlinh20192019@gmail.com>
2026-07-29 23:36:20 +0700

    no message

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import HomePage from '@components/HomePage/HomePage';
 
 function App() {
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,10 +6,7 @@ function Banner() {
     return (
         <div className={container}>
             <div className={content}>
-                <h1 className={title}>
-                    XStore Marseille04
-                    Demodddddddddddddddddddddddddddddddddddddddddddddddd
-                </h1>
+                <h1 className={title}>XStore Marseille04</h1>
                 <div className={des}>
                     Make yours celebrations even more special this years with
                     beautiful.
